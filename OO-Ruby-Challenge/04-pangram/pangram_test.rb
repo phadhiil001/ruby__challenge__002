5432f10b7a5429f7f9afec18b65a360dd00fe77f
phadhiil001 <phadhiil001@gmail.com>
2024-05-24 16:26:15 -0500

    modified:   OO-Ruby-Challenge/01-hello-world/hello_world_test.rb 	modified:   OO-Ruby-Challenge/02-hello-world-instance/hello_world_test.rb 	modified:   OO-Ruby-Challenge/03-leap/leap_test.rb 	modified:   OO-Ruby-Challenge/04-pangram/pangram_test.rb 	modified:   OO-Ruby-Challenge/05-grains/grains_test.rb 	new file:   OO-Ruby-Challenge/run_all_test.sh

--- OO-Ruby-Challenge/04-pangram/pangram_test.rb
+++ OO-Ruby-Challenge/04-pangram/pangram_test.rb
@@ -11,49 +11,49 @@ class PangramTest < Minitest::Test
   end
 
   def test_pangram_with_only_lower_case
-    skip
+    # skip
     str = 'the quick brown fox jumps over the lazy dog'
     assert Pangram.is_pangram?(str)
   end
 
   def test_missing_character_x
-    skip
+    # skip
     str = 'a quick movement of the enemy will jeopardize five gunboats'
     refute Pangram.is_pangram?(str)
   end
 
   def test_another_missing_character_x
-    skip
+    # skip
     str = 'the quick brown fish jumps over the lazy dog'
     refute Pangram.is_pangram?(str)
   end
 
   def test_pangram_with_underscores
-    skip
+    # skip
     str = 'the_quick_brown_fox_jumps_over_the_lazy_dog'
     assert Pangram.is_pangram?(str)
   end
 
   def test_pangram_with_numbers
-    skip
+    # skip
     str = 'the 1 quick brown fox jumps over the 2 lazy dogs'
     assert Pangram.is_pangram?(str)
   end
 
   def test_missing_letters_replaced_by_numbers
-    skip
+    # skip
     str = '7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog'
     refute Pangram.is_pangram?(str)
   end
 
   def test_pangram_with_mixed_case_and_punctuation
-    skip
+    # skip
     str = '"Five quacking Zephyrs jolt my wax bed."'
     assert Pangram.is_pangram?(str)
   end
 
   def test_pangram_with_non_ascii_characters
-    skip
+    # skip
     str = 'Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.'
     assert Pangram.is_pangram?(str)
   end
